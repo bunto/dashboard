64855d0eb8b61c9a4d5efe38c4828b1c401882f9
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-04-02 10:33:50 +0200

    Correct makeProject for bunto-mercenary

--- project.go
+++ project.go
@@ -12,7 +12,7 @@ var (
 	defaultProjects   = []*Project{
 		makeProject("bunto", "bunto/bunto", "ruby", "bunto"),
 		makeProject("bemoji", "bunto/bemoji", "master", "bemoji"),
-		makeProject("mercenary", "bunto/mercenary", "master", "mercenary"),
+		makeProject("mercenary", "bunto/mercenary", "master", "bunto-mercenary"),
 		makeProject("bunto-import", "bunto/bunto-import", "master", "bunto-import"),
 		makeProject("bunto-feed", "bunto/bunto-feed", "master", "bunto-feed"),
 		makeProject("bunto-sitemap", "bunto/bunto-sitemap", "master", "bunto-sitemap"),
